134ff290b8ed01c5169237b50619c035b3b57bef
ISRAEL FANTONI <127950758+Israel-ux440@users.noreply.github.com>
2024-03-19 23:04:50 -0300

    Add project files.

---
+++ Calculadora.ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Calculadora.ConsoleApp1
+//Nossa calculadora deve ter a possibilidade de fazer várias operações de soma
+
+// requisito 03
+//Nossa calculadora deve ter a possibilidade fazer várias operações de soma e de subtração
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            bool deveContinuar = true;
+
+            while (true) 
+            {
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara 2024");
+
+                Console.WriteLine("Digite 1 para Adicionar");
+                Console.WriteLine("Digite 2 para subtrair");
+
+                Console.WriteLine("Digite S para sair");
+
+                string operacao = Console.ReadLine();
+
+                if (operacao == "S")
+                {   
+                    
+                    break;
+                }
+
+                Console.WriteLine("Digite o primeiro número");
+
+                
+                string primeiroNumeroString = "";
+           
+
+                primeiroNumeroString = Console.ReadLine();
+
+                int primeiroNumero = Convert.ToInt32(primeiroNumeroString);
+
+
+                Console.WriteLine("Digite o segundo número");
+
+                string segundoNumeroString = Console.ReadLine();
+
+                int segundoNumero = Convert.ToInt32(segundoNumeroString);
+
+                int resultado = 0;
+
+                if (operacao == "1")
+                {
+                    resultado = primeiroNumero + segundoNumero;
+                }
+
+               else if (operacao == "2")
+                {
+                    resultado = primeiroNumero - segundoNumero;
+                }
+
+
+                Console.WriteLine("O resultado é: " + resultado);
+
+                Console.ReadLine();
+
+                Console.WriteLine("Você deseja sair da calculadora? (S/N)");
+
+                string resposta = Console.ReadLine();
+
+               
+               
+
+            }
+
+              
+            
+   
+
+
+        }
+    }
+}
